519fdb16498688cb80895c13ba0bba0b18abc999
Sharmin Pathan <sheryl.dr@gmail.com>
2019-12-06 13:37:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from keras import backend as K
 
 if __name__ == "__main__":
     # parameters
-    epoch = 100
+    epoch = 700
     hidden_size = 100
     env=Env()
     num_actions = env.n_cust #we need idx
@@ -34,7 +34,7 @@ if __name__ == "__main__":
             
             # apply action, get rewards and new state
             input_t, path_t, reward, done = env.act(int(np.argmax(action)))
-            
+            #print(input_t)
             
             if not env.was_zero:
                 total_reward=total_reward+reward
@@ -42,8 +42,30 @@ if __name__ == "__main__":
                 
                 agent.train()
                 
-        print("Epoch {:03d}/99 | Loss {:.4f} | Path_length {}".format(e, loss, total_reward))
+        print("Epoch {:03d}/100 | Path_length {}".format(e, total_reward))
         
         total_reward=0
+        
+    env.reset()
+    input_t, path_t = env.observe()
+    done=False
+    total_reward=0
+
+
+    while not done:
+        env.was_zero=False
+        input_tm1 = np.array(input_t)
+        path_tm1 = np.array(path_t)
+        action_tm1 = agent.predict(input_tm1, path_tm1)
+        input_t, path_t, reward, done = env.act(int(np.argmax(action_tm1)))
+        print(env.input_data)
+        while env.was_zero and not done:
+            action_tm1[int(np.argmax(action_tm1))]=0
+            input_t, path_t, reward, done = env.act(int(np.argmax(action_tm1)))
+        
+        print(reward)
+        total_reward=reward+total_reward
+
+    print("path_length = ", total_reward)
 
 
